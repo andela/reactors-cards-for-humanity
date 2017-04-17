9f345f199bd4a3e243042710129bc56f90c2530d
andela-wmumbi <winnie.mumbi@andela.com>
2017-04-17 22:20:37 +0300

    edit .travis.yml and all.js

--- config/env/all.js
+++ config/env/all.js
@@ -5,5 +5,5 @@ var keys = rootPath + '/keys.txt';
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
-      db: "mongodb://127.0.0.1/reactors"
+    db: process.env.MONGOHQ_URL
 };
