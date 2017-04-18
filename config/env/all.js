8469f179e9a95b5b082e994157bb057094cbb4ec
gangachris <ganga.chris@gmail.com>
2017-04-18 15:40:25 +0300

    update pacakges

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,10 @@
-var path = require('path'),
-rootPath = path.normalize(__dirname + '/../..');
-var keys = rootPath + '/keys.txt';
+const path = require('path');
+
+const rootPath = path.normalize(`${__dirname}/../..`);
+// const keys = `${rootPath}/keys.txt`;
 
 module.exports = {
-	root: rootPath,
-	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
+  root: rootPath,
+  port: process.env.PORT || 3000,
+  db: process.env.MONGOHQ_URL
 };
