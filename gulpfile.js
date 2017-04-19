c0fbaeacdff31ce6a88e5a11dc27ec77105963b1
Mercy-Muchai <mercy.muchai@andela.com>
2017-04-19 15:36:18 +0300

    [ChoreID #143412445] Fix default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,40 @@
 // Include gulp
 const gulp = require('gulp');
-
+const gutil = require('gulp-util');
 // Include gulp plugins
 const eslint = require('gulp-eslint');
-const sass = require('gulp-sass');
 const nodemon = require('gulp-nodemon');
-const bower = require('gulp-bower');
-// const syncbrowser = require(browser-sync);
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+// const mocha = require('gulp-mocha');
 
-// const mocha = require('gulp-mocha')
-
+// eslint task
+gulp.task('lint', () => {
+  gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+// nodemon task
 gulp.task('nodemon_file', () => {
-    nodemon({
-        script: 'server.js',
-        ext: 'js html',
-        env: { NODE_ENV: 'development' }
-    });
-
-    gulp.task('lint', () => gulp.src(['public/js/**/*.js', 'test/**/*.js', 'app/**/*.js', '!node_modules/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError()));
-    /* gulp.task('default', ['lint']);*/
-
-    gulp.task('sass', () => gulp.src('./sass/**/*.scss')
+  nodemon({
+    script: 'server.js',
+    ext: 'js html',
+    env: { NODE_ENV: 'development' } });
+});
+// sass task
+gulp.task('sass', () => gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('.public/css')));
-    gulp.task('sass:watch', () => {
-        gulp.watch('./sass/**/*.scss', ['sass']);
-    });
-    // Run install command for bower; used a custom update command "update"
-    gulp.task('runbower', () => bower({ cmd: 'update' }));
+gulp.task('sass:watch', () => {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+// gulp.task('watch', () => {
+//   gulp.watch(['app/**/*.js', 'public/js/**/*.js'], ['lint'])
+//     .on('change', browserSync.reload);
+// });
+
+// default task
+gulp.task('default', ['lint', 'nodemon_file', 'sass'], () => {
+  gutil.log('Gulp is running!');
 });
