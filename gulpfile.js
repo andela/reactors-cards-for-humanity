2a4a106f2fdb8ddb9a8e1e10eb2bef0333bcec63
Mercy-Muchai <mercy.muchai@andela.com>
2017-04-20 14:22:27 +0300

    [#143412445] Fix foot.jade paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Include gulp
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+
 // Include gulp plugins
 const eslint = require('gulp-eslint');
 const nodemon = require('gulp-nodemon');
@@ -17,6 +18,7 @@ gulp.task('lint', () => {
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
+
 // nodemon task
 gulp.task('nodemon_file', () => {
   nodemon({
@@ -26,27 +28,32 @@ gulp.task('nodemon_file', () => {
 });
 // sass task
 gulp.task('sass', () => gulp.src('./sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('.public/css')));
+.pipe(sass().on('error', sass.logError))
+.pipe(gulp.dest('.public/css')));
 gulp.task('sass:watch', () => {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+// watch task
 gulp.task('watch', () => {
   gulp.watch(['app/**/*.js', 'public/js/**/*.js'], ['lint'])
     .on('change', browserSync.reload);
 });
 
+// checking tests with mocha task
 gulp.task('mocha', () => gulp.src(['./test/**/*.js'], { read: false })
   .pipe(mocha({ reporter: 'spec' }))
   .pipe(istanbul.writeReports())
   .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
 );
 
+// run bower task
 gulp.task('bower', () => {
-  bower({ cmd: 'update' });
+  bower();
 });
 
 // default task
-gulp.task('default', ['lint', 'nodemon_file', 'sass', 'watch', 'mocha', 'bower'], () => {
+gulp.task('default', ['lint', 'sass', 'bower', 'watch', 'nodemon_file'], () => {
   gutil.log('Gulp is running!');
 });
+
