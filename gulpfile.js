f1afeb1998d4dfe6fc7be838ba9bf2feb550bf94
Mercy-Muchai <mercy.muchai@andela.com>
2017-04-20 16:42:23 +0300

    #143412445 Convert Grunt to Gulp (#13)
    * Installed gulp
    
    * [ChoreID #143412445 Convert from Grunt to Gulp]
    
    * [ChoreID #143412445] Fix default task
    
    * [ChoreID #143412445] Fix errors-run nodemon
    
    * [ChoreID #143412445] New Task:Run mocha
    
    * [ChoreID #143412445]Remove grunt dependencies.Ignore Gruntfile
    
    * [Chore #143412445]Add Bower task. Fix bug on scripts
    
    * [Chore #143412445] Make PR Recommendations
    
    * [#143412445] Fix foot.jade paths
    
    * [#143412445] Fix mccha task

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+// Include gulp
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+
+// Include gulp plugins
+const eslint = require('gulp-eslint');
+const nodemon = require('gulp-nodemon');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const mocha = require('gulp-mocha');
+const bower = require('gulp-bower');
+const istanbul = require('gulp-istanbul');
+
+// eslint task
+gulp.task('lint', () => {
+  gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+// nodemon task
+gulp.task('nodemon_file', () => {
+  nodemon({
+    script: 'server.js',
+    ext: 'js html',
+    env: { NODE_ENV: 'development' } });
+});
+// sass task
+gulp.task('sass', () => gulp.src('./sass/**/*.scss')
+.pipe(sass().on('error', sass.logError))
+.pipe(gulp.dest('.public/css')));
+gulp.task('sass:watch', () => {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+// watch task
+gulp.task('watch', () => {
+  gulp.watch(['app/**/*.js', 'public/js/**/*.js'], ['lint'])
+    .on('change', browserSync.reload);
+});
+
+// checking tests with mocha task
+gulp.task('test', () => gulp.src(['./test/**/*.js'], { read: false })
+  .pipe(mocha({ reporter: 'spec' }))
+  .pipe(istanbul.writeReports())
+  .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
+);
+
+// run bower task
+gulp.task('bower', () => {
+  bower();
+});
+
+// default task
+gulp.task('default', ['lint', 'sass', 'bower', 'watch', 'nodemon_file'], () => {
+  gutil.log('Gulp is running!');
+});
+
