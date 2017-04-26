90db5197c6cbdcf2299171ac8258d9aee0c95363
Ewanjiru <eunice.wanjiru@andela.com>
2017-04-26 19:36:16 +0300

    Chore/143412447/integrate coveralls (#21)
    * [chore #143412447] Add coverage button to readme
    
    * [chore #143412447] add coverage to ignore and configure coveralls
    
    * [chore #143412447] change coverage to coveralls
    
    * [chore #143412447] add to scripts for coveralls and test
    
    * [chore #143412447] add a npm command to run coveralls
    
    * [chore #143412447] add a coverall badge at the top of the file
    
    * [chore #143412447] add a coverall badge at the top of the file
    
    * [chore #143412447] change position of the coverall badge
    
    * [chore #143412447] uncomment on already commented lines
    
    * [chore #143412447] uncomment already commented lines
    
    * [chore #143412447] Add coverage button to readme
    
    * [chore #143412447] add coverage to ignore and configure coveralls
    
    * [chore #143412447] change coverage to coveralls
    
    * [chore #143412447] add to scripts for coveralls and test
    
    * [chore #143412447] add a npm command to run coveralls
    
    * [chore #143412447] add a coverall badge at the top of the file
    
    * [chore #143412447] add a coverall badge at the top of the file
    
    * [chore #143412447] change position of the coverall badge
    
    * [chore #143412447] uncomment on already commented lines
    
    * [chore #143412447] remove unnecessary lines & add coverage folder to ignore
    
    * [chore #143412447] remove trailing space detected by hound
    
    * [chore #143412447] correct merge conflicts on package.json,readme,travis
    
    * [chore #143412447] correct merge conflicts on package.json
    
    * [chore #143412447] correct merge conflicts on package.json
    
    * [chore #143412447] add yarn.lock to ignor
    
    * [chore #143412447] fix travis build
    
    * [chore #143412447] remove yarn from ignore file
    
    * [chore #143412447] Add coverage button to readme
    
    * [chore #143412447] add coverage to ignore and configure coveralls
    
    * [chore #143412447] change coverage to coveralls
    
    * [chore #143412447] add to scripts for coveralls and test
    
    * [chore #143412447] add a npm command to run coveralls
    
    * [chore #143412447] add a coverall badge at the top of the file
    
    * [chore #143412447] add a coverall badge at the top of the file
    
    * [chore #143412447] uncomment on already commented lines
    
    * [chore #143412447] add coverage to ignore and configure coveralls
    
    * [chore #143412447] add to scripts for coveralls and test
    
    * [chore #143412447] add a npm command to run coveralls
    
    * [chore #143412447] add a coverall badge at the top of the file
    
    * [chore #143412447] add a coverall badge at the top of the file
    
    * [chore #143412447] change position of the coverall badge
    
    * [chore #143412447] uncomment on already commented lines
    
    * [chore #143412447] remove unnecessary lines & add coverage folder to ignore
    
    * [chore #143412447] remove trailing space detected by hound
    
    * [chore #143412447] correct merge conflicts on package.json
    
    * [chore #143412447] correct merge conflicts on package.json
    
    * [chore #143412447] add yarn.lock to ignor
    
    * [chore #143412447] remove yarn from ignore file
    
    * [chore #143412447] add  line at end of file
    
    * [chore #143412447] remove trailing space on model.js

--- test/article/model.js
+++ test/article/model.js
@@ -1,17 +1,16 @@
-// /**
-//  * Module dependencies.
-//  */
+// Module dependencies.
+
 // var should = require('should'),
 //     app = require('../../server'),
 //     mongoose = require('mongoose'),
 //     User = mongoose.model('User'),
 //     Article = mongoose.model('Article');
 
-// //Globals
+// Globals
 // var user;
 // var article;
 
-// //The tests
+// The tests
 // describe('<Unit Test>', function() {
 //     describe('Model Article:', function() {
 //         beforeEach(function(done) {
@@ -22,6 +21,7 @@
 //                 password: 'password'
 //             });
 
+//             user.save(function(err) {
 //             user.save(function(err) {
 //                 article = new Article({
 //                     title: 'Article Title',
@@ -34,14 +34,14 @@
 //         });
 
 //         describe('Method Save', function() {
-//             it('should be able to save whithout problems', function(done) {
+//           it('should be able to save whithout problems', function(done) {
 //                 return article.save(function(err) {
 //                     should.not.exist(err);
 //                     done();
 //                 });
 //             });
 
-//             it('should be able to show an error when try to save witout title', function(done) {
+//           it('should be able to show an error when try to save witout title', function(done) {
 //                 article.title = '';
 
 //                 return article.save(function(err) {
@@ -55,4 +55,5 @@
 //             done();
 //         });
 //     });
+// });
 // });
