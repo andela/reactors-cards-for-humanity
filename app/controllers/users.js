cebff265a8b2af29a372df32633cc50da626f928
andela-wmumbi <winnie.mumbi@andela.com>
2017-04-27 20:14:43 +0300

    [feature #143412453] limit max players -implement api for searching users -limit maximum number of players

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,21 +1,21 @@
 /**
  * Module dependencies.
  */
-
-const mongoose = require('mongoose');
-
-const User = mongoose.model('User');
-const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose'),
+  User = mongoose.model('User');
 const avatars = require('./avatars').all();
 
-const secretKey = 'hello';
-// Auth callback
-exports.authCallback = (req, res, next) => {
+/**
+ * Auth callback
+ */
+exports.authCallback = function (req, res, next) {
   res.redirect('/chooseavatars');
 };
 
-// Show login form
-exports.signin = (req, res) => {
+/**
+ * Show login form
+ */
+exports.signin = function (req, res) {
   if (!req.user) {
     res.redirect('/#!/signin?error=invalid');
   } else {
@@ -23,9 +23,10 @@ exports.signin = (req, res) => {
   }
 };
 
-
-// Show sign up form
-exports.signup = (req, res) => {
+/**
+ * Show sign up form
+ */
+exports.signup = function (req, res) {
   if (!req.user) {
     res.redirect('/#!/signup');
   } else {
@@ -33,23 +34,27 @@ exports.signup = (req, res) => {
   }
 };
 
-// Logout
-
-exports.signout = (req, res) => {
+/**
+ * Logout
+ */
+exports.signout = function (req, res) {
   req.logout();
   res.redirect('/');
 };
 
-// Session
-exports.session = (req, res) => {
+/**
+ * Session
+ */
+exports.session = function (req, res) {
   res.redirect('/');
 };
 
-
-// Check avatar - Confirm if the user who logged in via passport |
-// already has an avatar. If they don't have one, redirect them |
-// to our Choose an Avatar page.
-exports.checkAvatar = (req, res) => {
+/**
+ * Check avatar - Confirm if the user who logged in via passport
+ * already has an avatar. If they don't have one, redirect them
+ * to our Choose an Avatar page.
+ */
+exports.checkAvatar = function (req, res) {
   if (req.user && req.user._id) {
     User.findOne({
       _id: req.user._id
@@ -67,8 +72,10 @@ exports.checkAvatar = (req, res) => {
   }
 };
 
-// Create user
-exports.create = (req, res) => {
+/**
+ * Create user
+ */
+exports.create = function (req, res) {
   if (req.body.name && req.body.password && req.body.email) {
     User.findOne({
       email: req.body.email
@@ -85,7 +92,6 @@ exports.create = (req, res) => {
               user
             });
           }
-
           req.logIn(user, (err) => {
             if (err) return next(err);
             return res.redirect('/#!/');
@@ -100,8 +106,10 @@ exports.create = (req, res) => {
   }
 };
 
-// Assign avatar to user
-exports.avatars = (req, res) => {
+/**
+ * Assign avatar to user
+ */
+exports.avatars = function (req, res) {
   // Update the current user's profile to include the avatar choice they've made
   if (req.user && req.user._id && req.body.avatar !== undefined &&
     /\d/.test(req.body.avatar) && avatars[req.body.avatar]) {
@@ -116,7 +124,7 @@ exports.avatars = (req, res) => {
   return res.redirect('/#!/app');
 };
 
-exports.addDonation = (req, res) => {
+exports.addDonation = function (req, res) {
   if (req.body && req.user && req.user._id) {
     // Verify that the object contains crowdrise data
     if (req.body.amount && req.body.crowdrise_donation_id && req.body.donor_name) {
@@ -143,9 +151,10 @@ exports.addDonation = (req, res) => {
   res.send();
 };
 
-
-// Show profile
-exports.show = (req, res) => {
+/**
+ *  Show profile
+ */
+exports.show = function (req, res) {
   const user = req.profile;
 
   res.render('users/show', {
@@ -154,13 +163,17 @@ exports.show = (req, res) => {
   });
 };
 
-// Send User
-exports.me = (req, res) => {
+/**
+ * Send User
+ */
+exports.me = function (req, res) {
   res.jsonp(req.user || null);
 };
 
-// Find user by id
-exports.user = (req, res, next, id) => {
+/**
+ * Find user by id
+ */
+exports.user = function (req, res, next, id) {
   User
     .findOne({
       _id: id
@@ -172,63 +185,12 @@ exports.user = (req, res, next, id) => {
       next();
     });
 };
-
-exports.signUpWithJWT = (req, res) => {
-  if (req.body.name && req.body.password && req.body.email) {
-    User.findOne({
-      email: req.body.email
-    }).exec((err, existingUser) => {
-      if (!existingUser) {
-        const user = new User(req.body);
-          // Switch the user's avatar index to an actual avatar url
-        user.avatar = avatars[user.avatar];
-        user.provider = 'jwt';
-        user.save((err) => {
-          if (err) {
-            return res.status(400).json({ message: 'Could not create user' });
-          }
-          const token = jwt.sign({ data: user._id }, secretKey, {
-            expiresIn: 60 * 60
-          });
-
-            // Send token
-          res.status(200).json(Object.assign({},
-        { id: user.id, name: user.name, email: user.email }, { token }));
-        });
-      } else {
-        return res.status(401).json({ message: 'Existing User' });
-      }
-    });
-  } else {
-    return res.status(401).json({ message: 'Name, email and password are required' });
-  }
-};
-exports.loginWithJWT = (req, res) => {
-    // Check if the email and passord fields are empty
-  if (req.body.email === '' || req.body.password === '') {
-    return res.status(401).json({ message: 'The email and password fields cannot be empty' });
-  } else if (!(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email))) {
-      // Check that the correct password format is entered
-    return res.status(401).json({ message: 'Please enter a valid email format' });
-  }
+exports.search = function (req, res, next) {
   User
-      .findOne({ email: req.body.email })
-      .then((user) => {
-        if (!user) {
-          return res.status(401).json({ message: 'User not found' });
-        }
-        if (!user.authenticate(req.body.password)) {
-          return res.status(401).json({
-            message: 'Invalid password'
-          });
-        }
-        // Generate and assign token to authenticated user
-        const token = jwt.sign({ data: user._id }, secretKey, {
-          expiresIn: 60 * 60
-        });
-        // Send token
-        user.password = null;
-        res.status(200).json(Object.assign({},
-        { id: user.id, name: user.name, email: user.email }, { token }));
-      });
+    .find({ name: { $regex: new RegExp(req.query.q, 'i') } }, 'email name')
+    .exec((err, users) => {
+      if (err) return next(err);
+      if (!users) return next(new Error('No users found '));
+      return res.status(200).send(users);
+    });
 };
